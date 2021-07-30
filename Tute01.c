dcc4c8a8daabb0c3b9ece014803d6202c000aba7
it21105098 Kiran <88177296+Suryaa-IT21105098@users.noreply.github.com>
2021-07-30 06:59:45 +0000

    tute 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
+  int n1, n2;
+  float avg = 0;
   
+printf("Enter Number 1 : ");
+scanf("%d", &n1);
+printf("Enter Number 1 : ");
+scanf("%d", &n2);
+avg = (float)n1/n2;
+printf("Average is : %f", avg);
+
   return 0;
 }
 
